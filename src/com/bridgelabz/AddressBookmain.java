375738f78340733a2854e0dc9e49256f27294f39
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-01 23:46:06 +0530

    Added: Ability to delete a person using person's name - Use Console to delete a person

--- src/com/bridgelabz/AddressBookmain.java
+++ src/com/bridgelabz/AddressBookmain.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
-public class AddressBookmain{
+import java.util.Scanner;
+
+public class AddressBookmain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program!");
+        AddressBook addressBook = new AddressBook();
+        // Add some sample contacts
+        addressBook.addPerson(new Person("Alice", "alice@example.com", "1234567890"));
+        addressBook.addPerson(new Person("Bob", "bob@example.com", "2345678901"));
+        addressBook.addPerson(new Person("Charlie", "charlie@example.com", "3456789012"));
+        // Delete a contact
+        addressBook.deletePerson("Bob");
+        // Display remaining contacts
+        addressBook.displayContacts();
     }
 }
